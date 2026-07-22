bec2bd68a2b255578f7c6965ea362b673938ac47
mohammadahmed41235-ship-it <mohammadahmed41235@gmail.com>
2026-07-22 17:42:55 +0500

    Milestone 6: comments system

--- apps/comments/tests.py
+++ apps/comments/tests.py
@@ -1,3 +1,136 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from apps.news.models import NewsArticle
+
+from .models import Comment
+
+
+User = get_user_model()
+
+
+class CommentTests(TestCase):
+
+    def setUp(self):
+        self.author = User.objects.create_user(
+            username="commenter",
+            password="testpassword123"
+        )
+
+        self.other_user = User.objects.create_user(
+            username="other",
+            password="testpassword123"
+        )
+
+        submitter = User.objects.create_user(
+            username="submitter",
+            password="testpassword123"
+        )
+
+        self.article = NewsArticle.objects.create(
+            title="Test Article",
+            source_url="https://example.com",
+            description="Testing comments",
+            submitted_by=submitter,
+        )
+
+
+    def test_authenticated_user_can_comment(self):
+        self.client.login(
+            username="commenter",
+            password="testpassword123"
+        )
+
+        response = self.client.post(
+            reverse("comments:add_comment", args=[self.article.pk]),
+            {
+                "body": "Good article"
+            }
+        )
+
+        self.assertEqual(response.status_code, 302)
+
+        comment = Comment.objects.first()
+
+        self.assertEqual(comment.author, self.author)
+        self.assertEqual(comment.article, self.article)
+
+
+    def test_author_can_edit_comment(self):
+        comment = Comment.objects.create(
+            article=self.article,
+            author=self.author,
+            body="Old comment"
+        )
+
+        self.client.login(
+            username="commenter",
+            password="testpassword123"
+        )
+
+        self.client.post(
+            reverse(
+                "comments:comment_edit",
+                args=[comment.pk]
+            ),
+            {
+                "body": "Updated comment"
+            }
+        )
+
+        comment.refresh_from_db()
+
+        self.assertEqual(
+            comment.body,
+            "Updated comment"
+        )
+
+
+    def test_other_user_cannot_edit_comment(self):
+
+        comment = Comment.objects.create(
+            article=self.article,
+            author=self.author,
+            body="Private comment"
+        )
+
+        self.client.login(
+            username="other",
+            password="testpassword123"
+        )
+
+        response = self.client.get(
+            reverse(
+                "comments:comment_edit",
+                args=[comment.pk]
+            )
+        )
+
+        self.assertEqual(response.status_code, 404)
+
+
+    def test_author_can_delete_comment(self):
+
+        comment = Comment.objects.create(
+            article=self.article,
+            author=self.author,
+            body="Delete me"
+        )
+
+        self.client.login(
+            username="commenter",
+            password="testpassword123"
+        )
+
+        self.client.post(
+            reverse(
+                "comments:comment_delete",
+                args=[comment.pk]
+            )
+        )
+
+        self.assertEqual(
+            Comment.objects.count(),
+            0
+        )
